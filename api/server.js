9f9dd23970ded984bfbce8b4fddd7c7b14aa0a41
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-20 22:46:38 -0400

    fixed a typo in index.js and confirmed that the server runs. also completed the authmiddleware.js

--- api/server.js
+++ api/server.js
@@ -4,19 +4,19 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 
+// const authRouter = require('../auth/authRouter.js');
+// const eventRouter = require('../events/eventRouter.js');
+// const userRouter = require('../users/userRouter.js');
+
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use(helmet());
 
-const authRouter = require('../auth/authRouter.js');
-const eventRouter = require('../events/eventRouter.js');
-const userRouter = require('../users/userRouter.js');
-
-server.use('/auth', authRouter);
-server.use('/events', eventRouter);
-server.use('/users', userRouter);
+// server.use('/api/auth', authRouter);
+// server.use('/api/events', eventRouter);
+// server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'Server up and running' });
--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,1 +1,24 @@
-// WIP
+// Complete
+
+const jwt = require('jsonwebtoken');
+const secret = require('../config/secrets.js');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secret.jwtSecret, (error, decodedToken) => {
+            if (error) {
+                res.status(401).json({ message: 'Wrong info.' });
+            } else {
+                req.user = {
+                    username: decodedToken.username,
+                };
+                next();
+            }
+        });
+    } else {
+        res.status(400).json({ message: 'No token provided.' });
+    };
+};
+

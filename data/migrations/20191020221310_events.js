752ffe30d11826ef0ad11d147ceca8d71a417d34
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-20 23:15:49 -0400

    completed schema for events and users tables

--- data/migrations/20191020221310_events.js
+++ data/migrations/20191020221310_events.js
@@ -1,9 +1,17 @@
-// WIP
+// Complete
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('events', table => {
+        table.increments();
+        table.string('Title', 128).notNullable();
+        table.string('Date', 128).notNullable();
+        table.string('Time', 128).notNullable();
+        table.string('Location', 128).notNullable();
+        table.string('Description', 512);
+        table.string('Image', 512);
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('events');
 };
--- data/migrations/20191020221322_users.js
+++ data/migrations/20191020221322_users.js
@@ -1,9 +1,16 @@
-// WIP
+// Complete
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', table => {
+        table.increments();
+        table.string('username', 128)
+            .notNullable()
+            .unique();
+        table.string('password', 128)
+            .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('users');
 };
